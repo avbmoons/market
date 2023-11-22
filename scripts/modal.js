277ab2fc55a8f681fe295064abcf425c15ca06a9
Elena-admin <avb.moons@gmail.com>
2023-11-22 21:40:22 +0300

    add layout and some components

---
+++ scripts/modal.js
@@ -0,0 +1,7 @@
+const modal = document.getElementsByClassName("modal")[0];
+const openModal = () => {
+  modal.style.display = "block";
+};
+const closeModal = () => {
+  modal.style.display = "none";
+};
