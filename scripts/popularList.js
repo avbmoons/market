b9465739c99348eefa64c8f90ec945e2b5e05f23
Elena-admin <avb.moons@gmail.com>
2024-01-16 22:12:22 +0300

    to cart from index

--- scripts/popularList.js
+++ scripts/popularList.js
@@ -31,16 +31,16 @@ class CatalogItem {
     this.isNew = isNew;
   }
   render() {
-    return `<div class="item-box">
+    return `<div data-id=${this.id} class="item-box">
       <img class="item-box__image"
         src="${this.image}"
         id="item-${this.id}"
-        alt="photo"/>
+        alt="photo" title="${this.name}/>
       <a class="item-box__heading" href="pages/product.html">${this.name}</a>
       <div class="item-box__bottom-box">
         <div class="stores">
             <p class="price">${this.priceActive}&nbsp;${this.currency}</p>
-            <button class="to-cart">Add to cart</button>
+            <button class="to-cart" id="toCart-${this.id}">Add to cart</button>
         </div>
       </div>
     </div>`;
